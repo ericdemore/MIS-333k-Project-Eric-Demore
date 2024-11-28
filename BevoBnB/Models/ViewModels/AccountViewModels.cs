173d651dee7116db9934546cd562cbf1fb1c4977
Antonio Meza <154790561+antoniowastaken@users.noreply.github.com>
2024-11-28 03:31:37 -0600

    Pushing user profile changes

--- BevoBnB/Models/ViewModels/AccountViewModels.cs
+++ BevoBnB/Models/ViewModels/AccountViewModels.cs
@@ -81,11 +81,6 @@ namespace BevoBnB.Models
     public class ChangePasswordViewModel
     {
         [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
-        public string OldPassword { get; set; }
-
-        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
