f6406ff525b309a7917d71e4391373c664445843
haifenglin2002 <159820373+haifenglin2002@users.noreply.github.com>
2024-11-29 21:36:44 -0600

    Review

---
+++ BevoBnB/Models/ViewModels/PropertyDetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace BevoBnB.ViewModels
+{
+    public class PropertyDetailsViewModel
+    {
+        public int PropertyID { get; set; }
+        public int PropertyNumber { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public List<ReviewViewModel> Reviews { get; set; }
+    }
+
+    public class ReviewViewModel
+    {
+        public string UserName { get; set; }
+        public int Rating { get; set; }
+        public string ReviewText { get; set; }
+    }
+}
